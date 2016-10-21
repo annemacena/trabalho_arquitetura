5f761bc11440e3f2a2bdd6e7a4e6ed68d40922c4
annemacena <arianne.macena@gmail.com>
2016-10-21 03:53:13 -0200

    Detalhes

--- assets/js/cpu.js
+++ assets/js/cpu.js
@@ -1,3 +1,13 @@
+/**
+ * 
+ * CPU Simulator
+ *
+ * 2016, Universidade Federal de Sergipe - UFS
+ * Arianne.
+ *
+ * Enjoy.
+ *
+ */
 $('#stop-cpu').attr("disabled", true);
 
 var mar = null;
@@ -33,7 +43,7 @@ var alu = document.getElementById('alu-in');
 
 var console_cpu = $("#progress-cpu");
 
-function start(){
+function start() {
     mar = 0;
     pc.value = '';
     ir.value = '';
@@ -45,7 +55,7 @@ function start(){
     avaliableAdresses = [];
     pc_count = 0;
     stop_interval = true;
-    $( "#start-cpu" ).tooltip('hide');
+    $("#start-cpu").tooltip('hide');
 
     console_cpu.empty();
     $('#start-cpu').attr("disabled", true);
@@ -53,8 +63,8 @@ function start(){
     $('#run-example').attr("disabled", true);
     $('#pause-cpu').attr("disabled", false);
 
-    for(i in addresses){
-        if(addresses[i].value == ""){
+    for (i in addresses) {
+        if (addresses[i].value == "") {
             addresses[i].disabled = true;
         } else {
             addresses[i].disabled = true;
@@ -64,34 +74,35 @@ function start(){
     }
     console_cpu.append("Iniciando CPU...");
     clock = setInterval(function() {
-        if(!isPaused){
-            if(stop_interval) {
+        if (!isPaused) {
+            if (stop_interval) {
                 clearInterval(clock);
                 stop();
             } else {
                 console_cpu.animate({
-                    scrollTop: console_cpu[0].scrollHeight}, 4000);
+                    scrollTop: console_cpu[0].scrollHeight
+                }, 4000);
 
                 decoder.value = '';
                 ir.value = '';
                 pc.value = avaliableAdresses[pc_count];
                 console_cpu.append("<br><span class='uc'>PC </span>aponta endereço: " + pc_count);
                 console_cpu.append(".<br><span class='cycle'>- Busca -</span>");
-                setTimeout(function(){
+                setTimeout(function() {
                     ir.value = addresses[avaliableAdresses[pc_count]].value;
                     console_cpu.append("<br><span class='uc'>IR</span> recebe operação.");
-                    setTimeout(function () {
+                    setTimeout(function() {
                         console_cpu.append("<br><span class='uc'>Unidade de controle</span> envia operação ao decodificador.");
                         cycle(addresses[avaliableAdresses[pc_count]].value);
-                        if(stop_interval || pc_count == (avaliableAdresses.length - 1)) {
-                            setTimeout(function(){
+                        if (stop_interval || pc_count == (avaliableAdresses.length - 1)) {
+                            setTimeout(function() {
                                 clearInterval(clock);
                                 stop_interval = true;
                                 stop();
                             }, 1500);
                         }
 
-                        if(operand_jump == null) {
+                        if (operand_jump == null) {
                             mar++;
                             pc_count = mar;
                         } else {
@@ -104,7 +115,7 @@ function start(){
     }, 5000);
 }
 
-function stop(){
+function stop() {
     console_cpu.append("<br>******* Fim do programa *******");
     stop_interval = true;
     $('#start-cpu').attr("disabled", false);
@@ -113,41 +124,41 @@ function stop(){
     $('#continue-cpu').attr("disabled", true);
     $('#pause-cpu').attr("disabled", true)
 
-    for(i in addresses){
+    for (i in addresses) {
         addresses[i].disabled = false;
     }
 }
 
-function cycle(opcode){
+function cycle(opcode) {
     console_cpu.append("<br><span class='cycle'>- Decodificação -</span>");
     opcode = opcode + mask.substring(0, mask.length - opcode.length);
     operation = opcode.substring(0, 3);
     operand_r = opcode.substring(3, 9);
-    operand_id = parseInt(opcode.substring(9,10));
+    operand_id = parseInt(opcode.substring(9, 10));
     operand = opcode.substring(10, 16);
 
-    switch(operation){                                      // OPERAÇÃO AND
+    switch (operation) { // OPERAÇÃO AND
         case '000':
             decoder.value = '';
             console_cpu.append("<br><span class='decoder'>Decodificador</span> identifica operação <strong>AND</strong>.");
             decoder.value = 'AND';
-            setTimeout(function () {
+            setTimeout(function() {
                 console_cpu.append("<br><span class='cycle'>- Execução -</span>");
                 console_cpu.append("<br><span class='alu'>ULA</span> recebe operador e operandos.");
                 alu.value = " & ";
                 var destination = decode_register(operand_r);
                 if (destination != null) {
-                    if(operand_id == 1) {
+                    if (operand_id == 1) {
                         var origin = decode_register(operand);
-                        if(origin != null){
+                        if (origin != null) {
                             destination.value = (origin.value.toString(2) | destination.value.toString(2));
-                            console_cpu.append("<br>* Registrador <strong>"+ registerBeingUsed +"</strong> recebe resultado da operação.");
+                            console_cpu.append("<br>* Registrador <strong>" + registerBeingUsed + "</strong> recebe resultado da operação.");
                         } else {
                             console_cpu.append("<br><span class='error'>Código de operando inválido.</span>");
                         }
                     } else {
                         output.value = (parseInt(operand, 2) & parseInt(destination.value, 2)).toString(2);
-                        console_cpu.append("<br>* Registrador <strong>"+ registerBeingUsed +"</strong> recebe resultado da operação.");
+                        console_cpu.append("<br>* Registrador <strong>" + registerBeingUsed + "</strong> recebe resultado da operação.");
                     }
                 } else {
                     console_cpu.append("<br><span class='error'>Código de operando inválido.</span>");
@@ -155,27 +166,27 @@ function cycle(opcode){
             }, 1000);
             break;
 
-        case '010':                                      // OPERAÇÃO OR
+        case '010': // OPERAÇÃO OR
             decoder.value = '';
             console_cpu.append("<br><span class='decoder'>Decodificador</span> identifica operação <strong>OR</strong>.");
             decoder.value = 'OR';
-            setTimeout(function () {
+            setTimeout(function() {
                 console_cpu.append("<br><span class='cycle'>- Execução -</span>");
                 console_cpu.append("<br><span class='alu'>ULA</span> recebe operador e operandos.");
                 alu.value = " | ";
                 var destination = decode_register(operand_r);
                 if (destination != null) {
-                    if(operand_id == 1) {
+                    if (operand_id == 1) {
                         var origin = decode_register(operand);
-                        if(origin != null){
+                        if (origin != null) {
                             destination.value = (origin.value.toString(2) | destination.value.toString(2));
-                            console_cpu.append("<br>* Registrador <strong>"+ registerBeingUsed +"</strong> recebe resultado da operação.");
+                            console_cpu.append("<br>* Registrador <strong>" + registerBeingUsed + "</strong> recebe resultado da operação.");
                         } else {
                             console_cpu.append("<br><span class='error'>Código de operando inválido.</span>");
                         }
                     } else {
                         output.value = (parseInt(operand, 2) | parseInt(destination.value, 2)).toString(2);
-                        console_cpu.append("<br>* Registrador <strong>"+ registerBeingUsed +"</strong> recebe resultado da operação.");
+                        console_cpu.append("<br>* Registrador <strong>" + registerBeingUsed + "</strong> recebe resultado da operação.");
                     }
                 } else {
                     console_cpu.append("<br><span class='error'>Código de operando inválido.</span>");
@@ -183,24 +194,24 @@ function cycle(opcode){
             }, 1000);
             break;
 
-        case '001':                                      // OPERAÇÃO MOV
+        case '001': // OPERAÇÃO MOV
             console_cpu.append("<br><span class='decoder'>Decodificador</span> identifica operação <strong>MOV</strong>");
             decoder.value = "MOV";
-            setTimeout(function(){
+            setTimeout(function() {
                 console_cpu.append("<br><span class='cycle'>- Execução -</span>");
                 var destination = decode_register(operand_r);
                 if (destination != null) {
-                    if(operand_id == 1) {
+                    if (operand_id == 1) {
                         var origin = decode_register(operand);
-                        if(origin != null){
+                        if (origin != null) {
                             destination.value = origin.value;
-                            $("#progress-cpu").append("<br>* Registrador <strong>"+ registerBeingUsed +"</strong> recebe valor.");
+                            $("#progress-cpu").append("<br>* Registrador <strong>" + registerBeingUsed + "</strong> recebe valor.");
                         } else {
                             $("#progress-cpu").append("<br><span class='error'>Código de operando inválido.</span>");
                         }
                     } else {
                         destination.value = parseInt(operand, 2);
-                        $("#progress-cpu").append("<br>* Registrador <strong>"+ registerBeingUsed +"</strong> recebe valor.");
+                        $("#progress-cpu").append("<br>* Registrador <strong>" + registerBeingUsed + "</strong> recebe valor.");
                     }
                 } else {
                     $("#progress-cpu").append("<br><span class='error'>Código de operando inválido.</span>");
@@ -208,40 +219,40 @@ function cycle(opcode){
             }, 1000);
             break;
 
-        case '100':                                      // OPERAÇÃO JMP
+        case '100': // OPERAÇÃO JMP
             console_cpu.append("<br><span class='decoder'>Decodificador</span> identifica operação <strong>JMP</strong>");
             decoder.value = "JMP";
-           // setTimeout(function () {
-                console_cpu.append("<br><span class='cycle'>- Execução -</span>");
-                operand_jump = opcode.substring(3, 16);
-                var result = parseInt(operand_jump, 2);
-                if(result >= 0 && result <= 4){
-                    pc_count = parseInt(operand_jump, 2);
-                } else {
-                    $("#progress-cpu").append("<br><span class='error'>Código de operando inválido.</span>");
-                }
-           // }, 500)
+            // setTimeout(function () {
+            console_cpu.append("<br><span class='cycle'>- Execução -</span>");
+            operand_jump = opcode.substring(3, 16);
+            var result = parseInt(operand_jump, 2);
+            if (result >= 0 && result <= 4) {
+                pc_count = parseInt(operand_jump, 2);
+            } else {
+                $("#progress-cpu").append("<br><span class='error'>Código de operando inválido.</span>");
+            }
+            // }, 500)
             break;
 
-        case '101':                                      // OPERAÇÃO ADD
+        case '101': // OPERAÇÃO ADD
             console_cpu.append("<br><span class='decoder'>Decodificador</span> identifica operação <strong>ADD</strong>");
-            setTimeout(function () {
+            setTimeout(function() {
                 console_cpu.append("<br><span class='cycle'>- Execução -</span>");
                 console_cpu.append("<br><span class='alu'>ULA</span> recebe operador e operandos.");
                 alu.value = " + ";
                 var destination = decode_register(operand_r);
                 if (destination != null) {
-                    if(operand_id == 1) {
+                    if (operand_id == 1) {
                         origin = decode_register(operand);
-                        if(origin != null){
+                        if (origin != null) {
                             destination.value = (parseInt(origin.value) + parseInt(destination.value));
-                            console_cpu.append("<br>* Registrador <strong>"+ registerBeingUsed +"</strong> recebe resultado da operação.");
+                            console_cpu.append("<br>* Registrador <strong>" + registerBeingUsed + "</strong> recebe resultado da operação.");
                         } else {
                             console_cpu.append("<br><span class='error'>Código de operando inválido.</span>");
                         }
                     } else {
                         output.value = (operand + destination.value);
-                        console_cpu.append("<br>* Registrador <strong>"+ registerBeingUsed +"</strong> recebe resultado da operação.");
+                        console_cpu.append("<br>* Registrador <strong>" + registerBeingUsed + "</strong> recebe resultado da operação.");
                     }
                 } else {
                     console_cpu.append("<br><span class='error'>Código de operando inválido.</span>");
@@ -249,25 +260,25 @@ function cycle(opcode){
             }, 1000);
             break;
 
-        case '011':                                      // OPERAÇÃO SUB
+        case '011': // OPERAÇÃO SUB
             console_cpu.append("<br><span class='decoder'>Decodificador</span> identifica operação <strong>SUB</strong>");
-            setTimeout(function () {
+            setTimeout(function() {
                 console_cpu.append("<br><span class='cycle'>- Execução -</span>");
                 console_cpu.append("<br><span class='alu'>ULA</span> recebe operador e operandos.");
                 alu.value = " - ";
                 var destination = decode_register(operand_r);
                 if (destination != null) {
-                    if(operand_id == 1) {
+                    if (operand_id == 1) {
                         var origin = decode_register(operand);
-                        if(origin != null){
+                        if (origin != null) {
                             destination.value = (parseInt(destination.value) - parseInt(origin.value));
-                            console_cpu.append("<br>* Registrador <strong>"+ registerBeingUsed +"</strong> recebe resultado da operação.");
+                            console_cpu.append("<br>* Registrador <strong>" + registerBeingUsed + "</strong> recebe resultado da operação.");
                         } else {
                             console_cpu.append("<br><span class='error'>Código de operando inválido.</span>");
                         }
                     } else {
                         destination.value = (destination.value - operand);
-                        console_cpu.append("<br>* Registrador <strong>"+ registerBeingUsed +"</strong> recebe resultado da operação.");
+                        console_cpu.append("<br>* Registrador <strong>" + registerBeingUsed + "</strong> recebe resultado da operação.");
                     }
                 } else {
                     console_cpu.append("<br><span class='error'>Código de operando inválido.</span>");
@@ -275,7 +286,7 @@ function cycle(opcode){
             }, 1000);
             break;
 
-        case '111':                                      // OPERAÇÃO HLT
+        case '111': // OPERAÇÃO HLT
             console_cpu.append("<br><span class='decoder'>Decodificador</span> identifica operação <strong>HLT</strong>");
             decoder.value = "HLT";
             console_cpu.append("<br><span class='cycle'>- Execução -</span>");
@@ -287,8 +298,8 @@ function cycle(opcode){
     }
 }
 
-function decode_register(value){
-    switch(value){
+function decode_register(value) {
+    switch (value) {
         case "100001":
             registerBeingUsed = "A";
             return register_A;
@@ -300,11 +311,11 @@ function decode_register(value){
     }
 }
 
-function put_example(){
-    var example =  $( "#examples" ).val();
-    switch(example){
+function put_example() {
+    var example = $("#examples").val();
+    switch (example) {
         case "add":
-            for(i in addresses){
+            for (i in addresses) {
                 addresses[i].value = "";
             }
             addresses[0].value = '0011000010000101';
@@ -313,7 +324,7 @@ function put_example(){
 
             break;
         case "sub":
-            for(i in addresses){
+            for (i in addresses) {
                 addresses[i].value = "";
             }
             addresses[0].value = '0011000010001111';
@@ -322,7 +333,7 @@ function put_example(){
 
             break;
         case "jmp":
-            for(i in addresses){
+            for (i in addresses) {
                 addresses[i].value = "";
             }
             addresses[0].value = '0011000010000111';
@@ -333,7 +344,7 @@ function put_example(){
 
             break;
         case "and":
-            for(i in addresses){
+            for (i in addresses) {
                 addresses[i].value = "";
             }
             addresses[0].value = '0011000010000100';
@@ -342,7 +353,7 @@ function put_example(){
 
             break;
         case "or":
-            for(i in addresses){
+            for (i in addresses) {
                 addresses[i].value = "";
             }
             addresses[0].value = '0011000010001010';
@@ -351,7 +362,7 @@ function put_example(){
 
             break;
         case "htl":
-            for(i in addresses){
+            for (i in addresses) {
                 addresses[i].value = "";
             }
             addresses[0].value = '0011000010000111';
@@ -359,7 +370,7 @@ function put_example(){
             addresses[2].value = '0011000100001101';
             break;
     }
-    $( "#start-cpu" ).tooltip('show');
+    $("#start-cpu").tooltip('show');
 }
 
 $('#pause-cpu').on('click', function(e) {
@@ -374,10 +385,10 @@ $('#continue-cpu').on('click', function(e) {
     $('#continue-cpu').attr("disabled", true);
     $('#pause-cpu').attr("disabled", false);
 });
-$(function () {
+$(function() {
     $('[data-toggle="tooltip"]').tooltip()
 })
 
-function clear_console(){
+function clear_console() {
     console_cpu.empty();
 }
